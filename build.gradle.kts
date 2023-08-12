6cfdeb2e256dc6208074aff6511f4bfc102bd978
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2023-08-12 17:05:03 +0100

    Add release property

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,9 @@ import java.util.*
 val props = Properties()
 file("build.properties").inputStream().use { props.load(it) }
 
+val release: String by properties
+
 allprojects {
     group = "org.cloudburstmc.fastutil"
-    version = props.getProperty("version")
+    version = props.getProperty("version") + if(release.toBoolean()) "" else "-SNAPSHOT"
 }
